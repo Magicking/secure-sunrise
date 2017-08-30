a046a5d51f90c3c2bcea1fe114bdd76d5fc9e03c
Magicking <s@6120.eu>
2017-08-30 12:57:35 +0200

    Add json dumper

--- maxmind.py
+++ maxmind.py
@@ -1,4 +1,5 @@
 import pickle
+import json
 from urllib.parse import urlparse
 import geoip2.database
 import simplekml
@@ -6,8 +7,11 @@ import simplekml
 kml=simplekml.Kml()
 
 cams = pickle.load(open('insecam_tz.p', 'rb'))
-reader = geoip2.database.Reader('GeoLite2-City_20170606/GeoLite2-City.mmdb')
+reader = geoip2.database.Reader('GeoLite2-City_20170606.mmdb')
+
+c_l = []
 for cam in cams:
+    c_l.append(cam[0])
     u = urlparse(cam[0])
     tz = cam[1]
     dsc = "Link: %s\nTZ: %s" % (cam[0], tz)
@@ -16,4 +20,5 @@ for cam in cams:
     kml.newpoint(name=host,
                  coords=[(info.location.longitude, info.location.latitude)],
                  description=dsc)
+print(json.dumps(c_l))
 kml.save('cams.kml')

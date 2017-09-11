d0a2ade337f543092f3b27b1782320270700e22a
Magicking <s@6120.eu>
2017-09-11 02:45:31 +0200

    Add javascript debug for first prototype

--- mockup/dist/js/riset.js
+++ mockup/dist/js/riset.js
@@ -3,8 +3,9 @@ var endpoint = '/api';
 setInterval(function(){
 $.ajax({url: endpoint + "/feeds?name=TODO",})
   .done(function( data ) {
-    $("#image0").attr("src", data[0]);
+    var url = data[Math.floor(Math.random() * data.length)];
+    $("#image0").attr("src", url);
+    console.log(url);
    timeout: 1000 //in milliseconds
-});
   });
 }, 10000);
